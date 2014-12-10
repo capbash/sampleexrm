ded4256dce294eb776e8a259bb10ad01d91542a0
Andrew Forward <aforward@gmail.com>
2014-12-09 22:16:06 -0500

    Working on connecting to remote nodes

---
+++ lib/sampleexrm/server.ex
@@ -0,0 +1,16 @@
+defmodule Sampleexrm.Server do
+  use GenServer
+
+  def start_link() do
+    :gen_server.start_link({:local, :sampleexrm}, __MODULE__, [], [])
+  end
+
+  def init([]) do
+    { :ok, [] }
+  end
+
+  def handle_call(:version, _from, state) do
+    { :reply, "0.0.4", state}
+  end
+
+end
